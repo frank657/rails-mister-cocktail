585cecb5165535761d110512257569d8d3eac7dd
frank zhu <qf.zhu.frank@gmail.com>
2019-03-28 11:42:18 +0800

    models added

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
